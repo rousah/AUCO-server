1340990ee7b71e8b313a2bc3ec3df7373eea8b37
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-03-15 11:05:55 +0100

    Delete mongoDB pass from git

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+var cors = require('cors');
 
 const app = express();
 
 // Connect Database
 connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
