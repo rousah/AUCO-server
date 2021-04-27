915a862eba47cc2ee8b48fdb02efcf6acee9a37d
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-04-28 01:04:54 +0200

    Log in for students working

--- controller/auth.js
+++ controller/auth.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const Class = require('../models/Class');
+const mongoose = require('mongoose');
 const { registerValidation, loginValidation } = require('../validation');
 const bcrypt = require('bcryptjs');
 const jsonwebtoken = require('jsonwebtoken');
+
+const checkPassword = async (user, password) => {
+    // Check if password is correct
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass) {
+        console.log("Password incorrect");
+        return false;
+    }
+    return true;
+}
 
 //Register
 router.post('/register', async (req, res) => {
@@ -66,17 +78,28 @@ router.post('/login', async (req, res) => {
         return res.status(400).send(error.details[0].message);
     }
 
-    // Check if the email exists
-    const user = await User.findOne({ user: req.body.email });
+    // Check if the email exists for teachers
+    let user = await User.findOne({ user: req.body.email });
     if (!user) {
-        console.log("Email is wrong");
-        return res.status(400).send("Email or password is wrong!");
+        // Check if user exists for students
+        user = await Class.find({
+            "students.username": req.body.email
+        },
+            {
+                "students.$": 1,
+                _id: 0
+            });
+        if (user.length == 0) {
+            console.log("No student or teacher with these credentials");
+            return res.status(400).send("Email, user or password is wrong!");
+        }
+        console.log("Found student");
+        user = user[0]['students'][0];
     }
 
     // Check if password is correct
-    const validPass = await bcrypt.compare(req.body.password, user.password);
+    const validPass = await checkPassword(user, req.body.password);
     if (!validPass) {
-        console.log("Password incorrect");
         return res.status(400).send("Email or password is wrong!");
     }
 
