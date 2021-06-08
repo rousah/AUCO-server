b43cfaf4db10152d6f4825ef53cd3d0ebd6674f6
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-06-08 16:48:17 +0200

    Get questionnaire done

--- controller/questionnaire.js
+++ controller/questionnaire.js
@@ -33,4 +33,30 @@ router.post('/create', async (req, res) => {
     }
 });
 
+// Get questionnaire (not tested)
+router.get('/:id', async (req, res) => {
+    console.log("/api/questionnaire/:id");
+
+    // Getting id info of the questionnaire
+    questionnaireId = req.params.id;
+
+    let thisQuestionnaire;
+    // Getting questionnaire
+    try {
+        thisQuestionnaire = await Questionnaire.find({ _id: questionnaireId });
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(404).json({ message: "Error: " + err }).send();
+    }
+    if (thisQuestionnaire.length > 0) {
+        console.log("Found questionnaire for " + questionnaireId);
+        return res.status(200).send(thisQuestionnaire[0]);
+    }
+    else {
+        console.log("No questionnaire found");
+        return res.status(404).json({ message: "No questionnaire for this questionnaireid" }).send();
+    }
+});
+
 module.exports = router;
