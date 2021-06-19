e90d68941eb26e96378b9686c217c019e3ccffee
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-06-19 10:14:49 +0200

    Check if questionnaire is or isn't partitionable

--- controller/questionnaire.js
+++ controller/questionnaire.js
@@ -77,93 +77,110 @@ router.get('/:id/random/:ids', async (req, res) => {
 
     const questions = thisQuestionnaire.questions;
 
-    // Getting responses of student
-    try {
-        const student = await getGamificationInfoOfStudent(ids);
+    // Check if questionnaire is partitionable or we should send all questions
+    if (thisQuestionnaire.partitionable) {
 
-        // Checking if response exists
-        console.log(student.responses);
-        let response = student.responses.filter(response => response.id_questionnaire == idq);
+        // Getting responses of student
+        try {
+            const student = await getGamificationInfoOfStudent(ids);
+
+            // Checking if response exists
+            let response = student.responses.filter(response => response.id_questionnaire == idq);
 
 
-        // Again, the same JSON 'undefined' error bullshit.......
-        let responseStr = JSON.stringify(response);
-        response = JSON.parse(responseStr);
+            // Again, the same JSON 'undefined' error bullshit.......
+            let responseStr = JSON.stringify(response);
+            response = JSON.parse(responseStr);
 
-        let selectedQuestions = [];
-        const questionLength = questions.length;
+            let selectedQuestions = [];
+            const questionLength = questions.length;
 
-        // If it exists
-        if (response[0] != undefined) {
-            // Get 5 random non answered questions
-            const answers = response[0];
+            // If it exists
+            if (response[0] != undefined) {
+                // Get 5 random non answered questions
+                const answers = response[0];
 
-            let numbers = [];
-            // Get numbers of unanswered questions
-            for (let i = 1; i <= questionLength; i++) {
-                // INSTEAD OF CHECKING IF NULL, CHECK IF NUMBER EXISTS
-                if (answers[i.toString()] == undefined) numbers.push(i);
-            }
+                let numbers = [];
+                // Get numbers of unanswered questions
+                for (let i = 1; i <= questionLength; i++) {
+                    // INSTEAD OF CHECKING IF NULL, CHECK IF NUMBER EXISTS
+                    if (answers[i.toString()] == undefined) numbers.push(i);
+                }
 
-            // Get random index value
-            function getRandomInt(max) {
-                return Math.floor(Math.random() * max);
-            }
+                // Get random index value
+                function getRandomInt(max) {
+                    return Math.floor(Math.random() * max);
+                }
 
-            if (numbers.length > 5) {
-                for (let i = 0; i < 5; i++) {
-                    // Get random index to use in array
-                    const randomIndex = getRandomInt(numbers.length);
+                if (numbers.length > 5) {
+                    for (let i = 0; i < 5; i++) {
+                        // Get random index to use in array
+                        const randomIndex = getRandomInt(numbers.length);
 
-                    // Get random question number from array of numbers of questions
-                    const random = numbers[randomIndex];
-                    console.log(random);
+                        // Get random question number from array of numbers of questions
+                        const random = numbers[randomIndex];
 
-                    // Set question number to random item
-                    questions[random - 1].questionNumber = random;
+                        // Set question number to random item
+                        questions[random - 1].questionNumber = random;
 
-                    // Add random item to selected
-                    selectedQuestions.push(questions[random - 1]);
+                        // Add random item to selected
+                        selectedQuestions.push(questions[random - 1]);
 
-                    // Delete chosen number from list of numbers
-                    numbers = numbers.filter(number => number != random);
+                        // Delete chosen number from list of numbers
+                        numbers = numbers.filter(number => number != random);
+                    }
+                }
+                else {
+                    for (let i = 0; i < numbers.length; i++) {
+                        const index = numbers[i];
+                        // Set question number to random item
+                        questions[index - 1].questionNumber = index;
+
+                        // Add random item to selected
+                        selectedQuestions.push(questions[index - 1]);
+                    }
                 }
             }
+            // If it doesn't exist
             else {
-                console.log(numbers)
-                for (let i = 0; i < numbers.length; i++) {
-                    const index = numbers[i];
-                    console.log(index);
-                    // Set question number to random item
-                    questions[index - 1].questionNumber = index;
+                // Get 5 random questions
+                // Get random index value
+                for (let i = 0; i < 5; i++) {
+                    const randomIndex = Math.floor(Math.random() * questionLength);
 
-                    // Add random item to selected
-                    selectedQuestions.push(questions[index - 1]);
+                    // Get random item
+                    selectedQuestions.push(questions[randomIndex]);
                 }
             }
-        }
-        // If it doesn't exist
-        else {
-            // Get 5 random questions
-            // Get random index value
-            for (let i = 0; i < 5; i++) {
-                const randomIndex = Math.floor(Math.random() * questionLength);
 
-                // Get random item
-                selectedQuestions.push(questions[randomIndex]);
-            }
+            // Add id's to selected questions
+            thisQuestionnaire.questions = selectedQuestions;
+            thisQuestionnaire.totalQuestions = questionLength;
+
+            console.log("Found questionnaire for " + idq);
+            return res.status(200).send(thisQuestionnaire);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(400).send("No student gamification for this studentId");
+        }
+    }
+
+    // If not partitionable
+    else {
+
+        // Add question number to questions
+        for (let i = 0; i < questions.length; i++) {
+
+            // Set question number to random item
+            questions[i].questionNumber = i + 1;
         }
 
-        // Add id's to selected questions
-        thisQuestionnaire.questions = selectedQuestions;
-        thisQuestionnaire.totalQuestions = questionLength;
+        // Replace questions with q number
+        thisQuestionnaire.questions = questions;
 
         console.log("Found questionnaire for " + idq);
         return res.status(200).send(thisQuestionnaire);
-    }
-    catch (err) {
-        console.log(err);
-        return res.status(400).send("No student gamification for this studentId");
     }
 });
 
